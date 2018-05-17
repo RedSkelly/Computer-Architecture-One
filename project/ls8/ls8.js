41f67eb3d3f9362582b124e6effe396eba235e12
Shaun Kelly <Shaun@redskelly.local>
2018-05-16 23:12:41 -0400

    step 7, un-hardcoded

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const RAM = require('./ram');
 const CPU = require('./cpu');
 
@@ -7,23 +8,26 @@ const CPU = require('./cpu');
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
-  // Hardcoded program to print the number 8 on the console
+  const program = [];
+  const fileName = process.argv[2];
+  const fileData = fs
+    .readFileSync(fileName, 'utf8')
+    .trim()
+    .split(/[\r\n]/);
 
-  const program = [
-    // print8.ls8
-    '10011001', // LDI R0,8  Store 8 into R0
-    '00000000', // R0        operandA for LDI instruction
-    '00001000', // 8         operandB for LDI instruction
-    '10011001', // LDI R1,9  Load R1 with value 9
-    '00000001', // R1
-    '00001001', // 9
-    '10101010', // MUL R0,R1 Multiply R0*R1, storing result in R0
-    '00000000', // R0
-    '00000001', // R1
-    '01000011', // PRN R0    Print the value in R0
-    '00000000', // R0        operandA for PRN register
-    '00000001', // HLT       Halt and quit
-  ];
+  for (let line of fileData) {
+    const comment = line.indexOf('#');
+
+    if (comment !== -1) {
+      line = line.substr(0, comment);
+    }
+    line = line.trim();
+
+    if (line === '') {
+      continue;
+    }
+    program.push(line);
+  }
 
   // Load the program into the CPU's memory a byte at a time
   for (let i = 0; i < program.length; i++) {
@@ -38,8 +42,11 @@ function loadMemory() {
 let ram = new RAM(256);
 let cpu = new CPU(ram);
 
-// TODO: get name of ls8 file to load from command line
-
-loadMemory(cpu);
+if (process.argv.length < 3) {
+  console.error('usage: node ls8 [fileName].ls8');
+  process.exit(1);
+} else {
+  loadMemory(cpu);
+}
 
 cpu.startClock();
