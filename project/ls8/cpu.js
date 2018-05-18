13cd4eb9ecfa24a8eff17aab2df20fab81320af6
Shaun Kelly <Shaun@redskelly.local>
2018-05-17 22:03:06 -0400

    implemented sys stack

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -5,6 +5,16 @@ const LDI = 0b10011001;
 const PRN = 0b01000011;
 const HLT = 0b00000001;
 const MUL = 0b10101010;
+const PUSH = 0b01001101;
+const DEC = 0b01111001;
+const POP = 0b01001100;
+const INC = 0b01111000;
+const JMP = 0b01010000;
+const CALL = 0b01001000;
+const RET = 0b00001001;
+
+const SP = 7; // stack pointer, R7 in register
+
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
@@ -19,6 +29,8 @@ class CPU {
 
     // Special-purpose registers
     this.PC = 0; // Program Counter
+
+    this.reg[SP] = 0xf4;
   }
 
   /**
@@ -62,6 +74,12 @@ class CPU {
         /* REVISIT: implement ALL ops above on Ln 52? or just MUL? */
         this.reg[regA] *= this.reg[regB];
         break;
+      case 'INC':
+        this.reg[regA]++;
+        break;
+      case 'DEC':
+        this.reg[regA]--;
+        break;
     }
   }
 
@@ -99,14 +117,30 @@ class CPU {
         // this.PC += 2;
         break;
 
+      case HLT:
+        this.stopClock();
+        break;
+
       case MUL:
         this.alu('MUL', operandA, operandB);
         // this.PC += 3;
         break;
 
-      case HLT:
-        this.stopClock();
+      case PUSH: // decrement first, see ls8-spec
+        this.alu('DEC', SP);
+        this.poke(this.reg[SP], this.reg[operandA]);
         break;
+
+      case POP: // increment last, see ls8-spec
+        this.reg[operandA] = this.ram.read(this.reg[SP]);
+        this.alu('INC', SP);
+        break;
+
+      // case JMP:
+
+      // case CALL:
+
+      // case RET:
 
       default:
         console.log(`Unknown instruction at ${this.PC}: ${IR.toString(2)}`);
@@ -118,7 +152,10 @@ class CPU {
     // instruction byte tells you how many bytes follow the instruction byte
     // for any particular instruction.
     // !!! IMPLEMENT ME
-    this.PC += (IR >> 6) + 1;
+    // should this be before the switch statement? check.
+    if (IR !== JMP && IR !== CALL && IR !== RET) {
+      this.PC += (IR >> 6) + 1;
+    }
   }
 }
 
